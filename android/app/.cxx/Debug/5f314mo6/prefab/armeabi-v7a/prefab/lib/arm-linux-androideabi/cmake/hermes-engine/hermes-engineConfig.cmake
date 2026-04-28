2258070ae950be4606dda7df701527a29befef7c
Funny_OFIC_FGA <danilimatov4@gmail.com>
2026-04-28 22:10:55 +0500

    chore: remove logs and build artifacts

--- android/app/.cxx/Debug/5f314mo6/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/danil/.gradle/caches/8.10.2/transforms/d8285b0758734ab2a7845bc121fbbd8d/transformed/jetified-hermes-android-0.78.2-debug/prefab/modules/libhermes/libs/android.armeabi-v7a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/danil/.gradle/caches/8.10.2/transforms/d8285b0758734ab2a7845bc121fbbd8d/transformed/jetified-hermes-android-0.78.2-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/RelWithDebInfo/1f584w4q/prefab/x86/prefab/lib/i686-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "C:/Users/danil/.gradle/caches/8.10.2/transforms/7b65d4e7c44250bdc5f38e0df48ea758/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/danil/.gradle/caches/8.10.2/transforms/7b65d4e7c44250bdc5f38e0df48ea758/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
